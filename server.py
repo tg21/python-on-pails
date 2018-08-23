fc8c1d772de997552cc68ea5e9a647ac75069fbe
unknown <gautamtushar21@gmail.com>
2018-08-23 16:50:22 +0530

    server can now run css

--- server.py
+++ server.py
@@ -18,45 +18,63 @@ import os
 #HTTPReuestHandler class
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-    #GET
-    def do_GET(self):
-        self.send_response(200)
-        
-        
-        message = "<html><head><title>pyServer</title></head><body>hello there</body></html>"
-        
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        print(self.path)
-        requested_file = "./public_html"+self.path
-        if Path(requested_file).is_file():
-            try:
-                file = open(requested_file,"r")
-                html_content = file.read()
-                file.close()
-            except Exception as e:
-                print("error ",e )
-                #html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"  
-                html_content = str(e)
-                
-        elif Path(requested_file).is_dir():
-            html_content = "<html><head><title>pyServer</title></head><body>"+str(os.listdir(requested_file))+"</body></html>"
-            
-        else:
-            self.send_response(400)
-            html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"
-            
-        
-        message = html_content
-        self.wfile.write(bytes(message,"utf8"))
-        return
-    
+	#GET
+	def do_GET(self):
+		#self.send_response(200)
+		
+		
+		message = "<html><head><title>pyServer</title></head><body>hello there</body></html>"
+		
+		#self.send_header('Content-type','text/html')
+		#self.end_headers()
+		print(self.path)
+		requested_file = "./public_html"+self.path
+		if Path(requested_file).is_file():
+			try:
+				file = open(requested_file,"r",)
+				html_content = file.read()
+				file.close()
+				self.send_response(200)
+				if requested_file.endswith('.css'):
+					self.send_header('Content-type','text/css')
+					self.end_headers()
+				elif requested_file.endswith('.jpg'):
+					self.send_header('Content-type','image/jpeg')
+					self.end_headers()
+				else:
+					self.send_header('Content-type','text/html')
+					self.end_headers()
+				
+			except Exception as e:
+				self.send_response(300)
+				self.send_header('Content-type','text/html')
+				self.end_headers()
+				print("error ",e )
+				#html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"  
+				html_content = str(e)
+				
+		elif Path(requested_file).is_dir():
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			html_content = "<html><head><title>pyServer</title></head><body>"+str(os.listdir(requested_file))+"</body></html>"
+			
+		else:
+			self.send_response(400)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"
+		
+		message = html_content
+		self.wfile.write(bytes(message,"utf8"))
+		return
+	
 def run():
-    print("Starting server ...")
-    server_address = ("0.0.0.0",80)
-    httpd = HTTPServer(server_address,testHTTPServer_RequestHandler)
-    print("running server at ",server_address)
-    httpd.serve_forever()
-    
-    
+	print("Starting server ...")
+	server_address = ("0.0.0.0",80)
+	httpd = HTTPServer(server_address,testHTTPServer_RequestHandler)
+	print("running server at ",server_address)
+	httpd.serve_forever()
+	
+	
 run()
