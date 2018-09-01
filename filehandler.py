4965453dd3f3d92c32c582ab9af5f3a821d68080
unknown <gautamtushar21@gmail.com>
2018-09-01 15:43:20 +0530

    server can now run python and php, and code is now more maintainable

---
+++ filehandler.py
@@ -0,0 +1,98 @@
+# this is helper file for server.py
+
+from pathlib import Path
+import os
+import subprocess
+
+#function to run python files
+def runPy(file):
+    proc = subprocess.Popen(['python', file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return str(proc.communicate()[0],"utf-8")
+
+#function to run php files
+
+def runPHP(file):
+    proc = subprocess.Popen(['php', file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return str(proc.communicate()[0],"utf-8")
+
+
+
+
+#function to read simple text files
+
+def read_text(file):
+    with open(file,"r") as nfile:
+        return nfile.read()
+    
+#function to read multimedia files
+
+def read_media(file):
+    with open(file,"rb") as nfile:
+        return nfile.read()
+    
+    
+#function to list directories files as a table
+def showDir(dirPath):
+    files = os.listdir(dirPath)
+    if "index.py" in files:
+        content = runPy(dirPath+"/index.py")
+    elif "index.php" in files:
+        content = runPHP(dirPath+"/index.php")
+    elif "index.html" in files:
+        content = read_text(dirPath+"/index.html")
+    else:
+        mimType = "text/html"
+        fileLinks = "<ul>"
+        for i in range(len(files)):         
+            #fileLinks += "<li><a href = "+dirPath+files[i]+">"+files[i]+"</a></li>"
+            fileLinks += "<li><a href = ./"+files[i]+">"+files[i]+"</a></li>"
+        content = "<html><head><title>PyOP</title></head><body>"+fileLinks+"</ul></body></html>"
+        
+    return content
+    
+    
+#function to handle requests
+
+def response(requested_file):
+    mimeType = "text/html"
+    isMedia = False #if file is media then we do not encode it
+    errorCode = 200
+    if Path(requested_file).is_file():
+        try:
+            if requested_file.endswith('.css'):
+                content = read_text(requested_file)
+                mimeType = "text/css"
+            elif requested_file.endswith('.jpg'):
+                content = read_media(requested_file)
+                mimeType = "image/jpeg"
+                isMedia = True
+            elif requested_file.endswith(".js"):
+                content = read_text(requested_file)
+                mimeType = "application/javascript"
+            elif requested_file.endswith("py"):
+                content = runPy(requested_file)
+                mimeType = "text/html"
+            elif requested_file.endswith(".php"):
+                content = runPHP(requested_file)
+                mimeType = "text/html"
+            else:
+                content = read_text(requested_file)
+                mimeType = "text/html"
+
+        except Exception as e:
+            mimeType = "text/html"
+            print("error ::-- ",e )
+            content = str(e)
+            errorCode = 300
+
+    elif Path(requested_file).is_dir():
+        content = showDir(requested_file)
+        mimeType = "text/html"
+        errorCode = 200
+
+    else:
+        errorCode  = 200 #wildcard response
+        content = read_text("./default/404.html")
+        mimeType = "text/html"
+        
+    return content,mimeType,errorCode,isMedia
--- server.py
+++ server.py
@@ -2,83 +2,23 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 import os
 import sys
-
+from filehandler import showDir,response
 #HTTPReuestHandler class
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):		
-	#function to read/write html
-	def read_text(self,file):
-		with open(file,"r") as new_file:
-			return new_file.read()
-		
-	#function to read binary files		
-	def read_images(self,file):
-		with open(file,"rb") as new_file:
-			#return str.encode(new_file.read())
-			return new_file.read()
-
 	#GET
 	def do_GET(self):
-		#self.send_response(200)
-		
-		
-		message = "<html><head><title>pyServer</title></head><body>hello there</body></html>"
-		isMedia = False
-		
-		#self.send_header('Content-type','text/html')
-		#self.end_headers()
 		print(self.path)
 		requested_file = "./public_html"+self.path
-		if Path(requested_file).is_file():
-			try:
-				#file = open(requested_file,"r",)
-				#html_content = file.read()
-				#file.close()
-				self.send_response(200)
-				if requested_file.endswith('.css'):
-					html_content = self.read_text(requested_file)
-					self.send_header('Content-type','text/css')
-					self.end_headers()
-				elif requested_file.endswith('.jpg'):
-					html_content = self.read_images(requested_file)
-					self.send_header('Content-type','image/jpeg')
-					#self.send_header("Content-length", os.path.getsize(requested_file))
-					#self.send_header("content-length",sys.getsizeof(html_content))
-					self.end_headers()
-					isMedia = True
-				elif requested_file.endswith(".js"):
-					html_content = self.read_text(requested_file)
-					self.send_header('content-type','application/javascript')
-				else:
-					html_content = self.read_text(requested_file)
-					self.send_header('Content-type','text/html')
-					self.end_headers()
-				
-			except Exception as e:
-				self.send_response(300)
-				self.send_header('Content-type','text/html')
-				self.end_headers()
-				print("error ",e )
-				#html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"  
-				html_content = str(e)
-				
-		elif Path(requested_file).is_dir():
-			self.send_response(200)
-			self.send_header('Content-type','text/html')
-			self.end_headers()
-			html_content = "<html><head><title>pyServer</title></head><body>"+str(os.listdir(requested_file))+"</body></html>"
-			
-		else:
-			self.send_response(400)
-			self.send_header('Content-type','text/html')
-			self.end_headers()
-			html_content = "<html><head><title>pyServer</title></head><body>404 : not found</body></html>"
-		
-		message = html_content
+		#requested_file = "."+self.path+"/"
+		content,mimeType,errorCode,isMedia = response(requested_file)
+		self.send_response(errorCode)
+		self.send_header('content-type',mimeType)
+		self.end_headers()
 		if isMedia:
-			self.wfile.write(message)
+			self.wfile.write(content)
 		else:
-			self.wfile.write(bytes(message,"utf8"))
+			self.wfile.write(bytes(content,"utf8"))
 		return
 	
 def run():
