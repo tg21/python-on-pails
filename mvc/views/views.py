3711eb10ab665e462f1e161c2eb5e904c156a20e
tg21 <gautamtushar21@gmail.com>
2021-04-07 21:11:57 +0530

    fixed type

--- mvc/views/views.py
+++ mvc/views/views.py
@@ -3,7 +3,7 @@
 Specify view routes in this file.
 give path to html file, python file or python functions.
 no parameters will be passed to python files or functions.(use routes.py in controllers folder if you need to do so)
-results obatined from these files witll be retured as text/html
+results obatined from these files will be retured as text/html
 ************************************************************************
 remove this placeholder routes and make your own
 =======================================================================
